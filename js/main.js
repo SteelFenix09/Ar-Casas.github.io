858846df186defb7e0619f1e344ccc7c6d12be9e
SteelFenix09 <f_771@outlook.com>
2026-01-02 16:44:29 -0600

    mejoras en los textos

--- js/main.js
+++ js/main.js
@@ -1,99 +1,131 @@
 //Elementos del DOM
 const startScreen = document.getElementById('start-screen');
+const modelScreen = document.getElementById('model-screen');
 const arContainer = document.getElementById('ar-container');
 const startButton = document.getElementById('start-button');
-const instructions = document.getElementById('instructions');
-const resetBtn = document.getElementById('reset-btn');
+const activateArBtn = document.getElementById('activate-ar-btn');
+const backBtn = document.getElementById('back-btn');
+const exitArBtn = document.getElementById('exit-ar-btn');
+const instructions = document.getElementById('ar-instructions');
+const resetBtn = document.getElementById('reset-btn-ar');
 const modelViewer = document.getElementById('house-modal');
+const modelViewerAr = document.getElementById('house-modal-ar');
 
-//Inicia experiencia AR
-startButton.addEventListener('click', () => {
-    console.log('Iniciando experiencia AR... ');
+//ver modelo estatico
+startButton.addEventListener('click',()=>{
+    console.log('Mostrando modelo 3d...');
+    startScreen.style.display = 'none'
+    modelScreen.style.display = 'block'
+})
 
-    // Cambiar pantallas
-    startScreen.style.display = 'none';
-    arContainer.style.display = 'block';
+//volver al inicio
+backBtn.addEventListener('click',()=>{
+    modelScreen.style.display = 'none'
+    startScreen.style.display = 'block'
+})
 
-    // Forzar modo AR despues de un breve retraso
-    setTimeout(() => {
-        console.log('Buscando boton AR...');
-        //Busca en shadowRoot del model-viewer
-        if (modelViewer.shadowRoot) {
-            const arButton = modelViewer.shadowRoot.querySelector('button[slot="ar-button"]');
-            if (arButton) {
-                console.log('Boton AR encontrado, haciendo clic...');
+//activar ar camara
+activateArBtn.addEventListener('click',()=>{
+    console.log('Modo AR');
+    modelScreen.style.display = 'none'
+    arContainer.style.display = 'block'
+
+    // entra en modo ar despues de un retraso
+
+    setTimeout(()=>{
+        console.log('Boton ar..');
+        if(modelViewerAr.shadowRoot){
+            const arButton = modelViewerAr.shadowRoot.querySelector('button[slot="ar-button"]');
+            if(arButton){
+                console.log('Boton ar encontrado, haciendo click');
                 arButton.click()
-            } else {
-                console.warn('Boton AR no encontado en shadowRoot');
-                const fallbackButton = document.querySelector('.ar-button')
-                if (fallbackButton) {
-                    fallbackButton.click();
-                }
+                //oculta instruciones despues de activar ar
+                setTimeout(()=>{
+                    if(instructions){
+                        instructions.classList.add('hidden');
+                    }
+                },5000)
+            } else{
+                console.warn('Boton ar no encontrado');
+                
             }
         }
-        setTimeout(() => {
-            if (instructions) {
-                instructions.classList.add('hide')
-            }
-        },8000)
-    },1000)
-}) 
+    },500)
+})
+
+//salir de Ar
+exitArBtn.addEventListener('click',()=>{
+    console.log('saliendo de AR..')
+    arContainer.style.display = 'none'
+    modelScreen.style.display = 'block'
+
+    modelViewerAr.ar = false;
+    if(instructions){
+        instructions.classList.remove('hidden');
+    }
+})
+
+// reubicar en ar
 
 resetBtn.addEventListener('click',()=>{
-    console.log('Reiniciando experiencia...');
-    
-    if(instructions){
-        instructions.classList.remove('hide')
-    }
+    console.log('Reubicando modelo ...');
+    modelViewerAr.ar = false
 
-    modelViewer.ar = false
     setTimeout(()=>{
-        modelViewer.ar = true
+        modelViewerAr.ar = true
+        if(instructions){
+            instructions.classList.remove('hidden');
+        }
 
-        setTimeout(() => {
-           if(modelViewer.shadowRoot){
-            const arButton = modelViewer.shadowRoot.querySelector('button[slot="ar-button"]')
-            if(arButton){
-                arButton.click()
+        setTimeout(()=>{
+            if(modelViewerAr.shadowRoot){
+                const arButton = modelViewerAr.shadowRoot.querySelector('button[slot="ar-button"]');
+                if(arButton){
+                    arButton.click()
+                }
             }
-           } 
-        },500 );
+        },500)
     },100)
 })
 
-modelViewer.addEventListener('ar-status', (event)=>{
-    console.log('Estado AR: ', event.detail.status);
-
-    if(event.detail.status === 'session-started'){
-        console.log('Sesion AR iniciada - Escaneando terreno...');
-        //Ocultar instrucciones cuando la sesion inicie
+//eventos ar
+modelViewerAr.addEventListener('ar-status',(event)=>{
+    console.log('Estado Ar: ',event.detail.status);
+    
+    if(event.detail.status ==='session-started'){
+        console.log('Sesion ar inicada - Escaneando terreno..');
         if(instructions){
-            instructions.classList.add('hidden')
+            setTimeout(()=>{
+                instructions.classList.add('hidden');
+            },3000)
         }
     }
-    if (event.detail.status === 'failed'){
-        console.error('Fallo la sesion AR:', event.detail.statusMessage);
-        alert('No se pudp iniciar la camara AR. Asegurese de dar permisos.')
+    if(event.detail.status ==='failed'){
+        console.error('Fallo la sesion',event.detail.statusMessage);
+        alert('No se pudo inicar la cmara AR')
     }
 })
 
-modelViewer.addEventListener('click',()=>{
-    console.log('Usuario toco la pantalla para colocar modelo');
-    if(instructions && !instructions.classList.contains('hidden')){
-        instructions.classList.add('hidden')
+// Ocultar instrucciones al tocar para colocar modelo
+modelViewerAR.addEventListener('click', () => {
+    console.log('Usuario tocó la pantalla para colocar modelo');
+    if (arInstructions && !arInstructions.classList.contains('hidden')) {
+        arInstructions.classList.add('hidden');
     }
-})
+});
 
-document.addEventListener('touchmove', (e)=>{
-    if(e.target === modelViewer || modelViewer.contains(e.target)){
-        e.preventDefault()
+// Prevenir scroll en modelo AR
+document.addEventListener('touchmove', (e) => {
+    if (e.target === modelViewerAR || modelViewerAR.contains(e.target)) {
+        e.preventDefault();
     }
-}, {passive: false})
+}, { passive: false });
 
-const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent)
-const isAndroid = /Android/.test(navigator.userAgent)
+// Detectar dispositivo
+const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+const isAndroid = /Android/.test(navigator.userAgent);
 
-console.log(`Dispositivo: ${isIOS ? 'iOS' : isAndroid ? 'Android' : 'Desktop'} `);
+console.log(`Dispositivo: ${isIOS ? 'iOS' : isAndroid ? 'Android' : 'Desktop'}`);
 
 if (isIOS) {
     console.log('📱 iOS detectado - Usando Quick Look');
